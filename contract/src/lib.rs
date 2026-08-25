116116a9f756c2ba5507d762f4c881479d62bef9
nafsonig <saurinkudelivery@gmail.com>
2026-08-25 17:30:45 +0100

    Session struct storage mapping

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -99,6 +99,7 @@ pub mod coaches {
         let platform_state = &mut ctx.accounts.platform_state;
         platform_state.admin = ctx.accounts.signer.key();
         platform_state.platform_fee_bps = initial_fee_bps;
+        platform_state.session_counter = 0;
         
         emit!(PlatformFeeUpdated {
             previous_fee: 0,
